dc6d85b1866e89ddc56d96f93c247d484c8deb61
George Alexiou <george@alexiou.xyz>
2019-07-26 19:28:49 +0300

    Finalized the script for averagereviews.sh. Now it will display only two decimal places in the output of the average overall scores.

--- averagereviews.sh
+++ averagereviews.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 cd "$1" || exit 1
-: ' By using the above command we navigate to the directory specified
-	by the argument (filename) passed to the script when running it from
-	the terminal. 
-'
 
-grep "<Overall>" hotel_*.dat | sed 's/\.dat:<Overall>/ /' | awk '{sum[$1] += $2; counts[$1]++;} END {for (i in sum) print i, sum[i]/counts[i];}'| sort -nrk2
+grep "<Overall>" hotel_*.dat | sed 's/\.dat:<Overall>/ /' |  awk '{sum[$1] += $2; counts[$1]++;} END {for (i in sum) printf "%s %.3g\n",  i, sum[i]/counts[i];}'  | sort -nrk2
