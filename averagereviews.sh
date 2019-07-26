eb9257176d61a08e3ff885bb954c54f82364e028
George Alexiou <george@alexiou.xyz>
2019-07-26 19:32:06 +0300

    Added comments to averagereviews.sh.

--- averagereviews.sh
+++ averagereviews.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#goes to directory as declared by the argument passed
 cd "$1" || exit 1
 
+#ranks the hotels depending on their average overall review score
 grep "<Overall>" hotel_*.dat | sed 's/\.dat:<Overall>/ /' |  awk '{sum[$1] += $2; counts[$1]++;} END {for (i in sum) printf "%s %.3g\n",  i, sum[i]/counts[i];}'  | sort -nrk2
