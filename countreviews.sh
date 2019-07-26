b7a0503b009354c458f339306aa432b4445f9622
George Alexiou <george@alexiou.xyz>
2019-07-26 19:33:08 +0300

    Modified countreviews.sh.
    Added comments and improved the code

--- countreviews.sh
+++ countreviews.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
+#goes to directory as declared by the argument passed
 cd "$1" || exit 1
-: ' By using the above command we navigate to the directory specified
-	by the argument (filename) passed to the script when running it from
-	the terminal. 
-'
 
+#ranks hotels based on the number of reviews they have
 grep -c Author hotel_*.dat | sort -t : -k2nr | sed 's/\.dat:/ /'
-: ' Using the grep -c command we can search all the hotel files and find
-	the number of lines that contain "Author" which will give us the number
-	of reviews. We then sort the data output by the grep command. Using the sed
-	command we change the output of the data from hotel_218100.dat:52 to
-	hotel_218100 52
-'
